fe8ac6282e2e77c2a65b06c08aed0c46e36fb8f1
Joe <joefuller042@gmail.com>
2023-06-09 18:44:34 +0100

    pretty sure this one is a step backwards

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "root",
-  password: "",
+  password: "sqlpass",
   database: "recipe_app",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = connection;
