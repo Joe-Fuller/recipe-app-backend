b2eb7de120360915843a2462a32c2fc87e4303c2
Joe <joefuller042@gmail.com>
2023-06-09 18:36:31 +0100

    i think this experiment might have failed

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,29 @@
 const mysql = require("mysql2/promise");
+const ENV = process.env.NODE_ENV || "development";
 
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
 });
 
-module.exports = connection;
+if (!process.env.MYSQL_DATABASE && !process.env.MYSQL_URL) {
+  throw new Error("MYSQL_DATABASE or MYSQL_URL not set");
+}
+
+const config =
+  ENV === "production"
+    ? {
+        connectionLimit: 10,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+      }
+    : {
+        connectionLimit: 10,
+        host: "localhost",
+        user: "root",
+        password: "sqlpass",
+        database: process.env.MYSQL_DATABASE || "recipe_app",
+      };
+
+module.exports = mysql.createPool(config);
