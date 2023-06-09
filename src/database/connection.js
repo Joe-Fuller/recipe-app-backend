1d7d6d55e021732779dff23ccdedd1e10f3a30e2
Joe <joefuller042@gmail.com>
2023-06-09 18:41:50 +0100

    a

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,31 +1,10 @@
 const mysql = require("mysql2/promise");
-const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
+const connection = mysql.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "recipe_app",
 });
 
-console.log(process.env);
-
-if (!process.env.MYSQL_DATABASE && !process.env.MYSQL_URL) {
-  throw new Error("MYSQL_DATABASE or MYSQL_URL not set");
-}
-
-const config =
-  ENV === "production"
-    ? {
-        connectionLimit: 10,
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-      }
-    : {
-        connectionLimit: 10,
-        host: "localhost",
-        user: "root",
-        password: "sqlpass",
-        database: process.env.MYSQL_DATABASE || "recipe_app",
-      };
-
-module.exports = mysql.createPool(config);
+module.exports = connection;
