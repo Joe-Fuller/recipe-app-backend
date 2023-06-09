dbb5b69ee77d4fcb13cdc5e29890093604a0d8e5
Joe <joefuller042@gmail.com>
2023-06-09 18:22:25 +0100

    try this

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use((req, res) => {
   res.status(404).json({ error: "Not found" });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
