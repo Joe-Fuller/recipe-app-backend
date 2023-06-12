bf1c25de21862114d17cec6c3a7504e364bab881
Joe <joefuller042@gmail.com>
2023-06-12 12:28:30 +0100

    a

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createPool({
   host: "recipe-app.cyclic.app",
   user: "joe",
   password: "sqlpassword",
-  database: "precious-pike-smockCyclicDB",
+  database: "precious-pike-smockCyclicD",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
