bd6d2eafca50dfc1b0fbf2b0da7cd9f3dfe69ab3
Joe <joefuller042@gmail.com>
2023-06-12 18:05:54 +0100

    fix splitIngredientString.js

--- src/utils/splitIngredientString.js
+++ src/utils/splitIngredientString.js
@@ -14,20 +14,23 @@ function splitIngredientString(ingredientString) {
     console.log(char, letterOrSpace.test(char));
 
     if (!letterOrSpace.test(char)) {
+      console.log("its amount");
       amount += char;
       return;
     }
 
     if (!unitsDone) {
       if (char !== " ") {
+        console.log("its units");
         units += char;
         return;
       } else {
+        console.log("units done");
         unitsDone = true;
         return;
       }
     }
-
+    console.log("its name");
     name += char;
   });
 
