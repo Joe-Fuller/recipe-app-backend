5e91183b073dece8feca28d0794a04818d63a740
Joe <joefuller042@gmail.com>
2023-06-12 10:52:07 +0100

    adas

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,21 @@
 const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "sqlpass",
+  host: "https://recipe-app.cyclic.app/",
+  user: "joe",
+  password: "sqlpassword",
   database: "recipe_app",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Database connection failed");
+  } else {
+    console.log("connected to database");
+  }
 });
 
 module.exports = connection;
