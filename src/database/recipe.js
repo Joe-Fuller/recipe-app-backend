c06f39ca39e5d3de62e3a81c4b42f5a026c7ce0f
Joe <joefuller042@gmail.com>
2023-06-12 12:33:06 +0100

    a

--- src/database/recipe.js
+++ src/database/recipe.js
@@ -4,6 +4,7 @@ const connection = require("./connection");
 async function getAllRecipes() {
   console.log("getting recipes");
   console.log(connection);
+  console.log(connection.config);
   try {
     console.log("in the model");
     const [rows] = await connection.query("SELECT * FROM Recipe");
