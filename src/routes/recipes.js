54cc806a2efaae1d784da2e4fe476ef7dcbb55ec
Joe <joefuller042@gmail.com>
2023-06-13 10:31:06 +0100

    big fixing

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -23,6 +23,7 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     console.log("posting");
+    console.log(req);
     console.log(scrapeRecipeFromUrl(url));
     const recipeData = await scrapeRecipeFromUrl(url);
     console.log("scraped", recipeData);
