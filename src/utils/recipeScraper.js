ee49bf1fd96631851cad0de36c5987ea5b8b78df
Joe <joefuller042@gmail.com>
2023-06-14 13:12:47 +0100

    general purpose script finder

--- src/utils/recipeScraper.js
+++ src/utils/recipeScraper.js
@@ -69,7 +69,7 @@ function findScriptWithSchema($) {
   for (let i = 0; i < scriptTags.length; i++) {
     if (i === 3) {
       console.log("====================");
-      console.log(scriptTags[i].text());
+      console.log($(scriptTags[i]));
       console.log("====================");
     }
     const scriptContent = $(scriptTags[i]).html();
