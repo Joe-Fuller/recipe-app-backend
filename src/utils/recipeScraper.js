5f0c65b9dea016d2691e2655ece6c4981aa605fa
Joe <joefuller042@gmail.com>
2023-06-14 13:31:36 +0100

    trying for site number 2

--- src/utils/recipeScraper.js
+++ src/utils/recipeScraper.js
@@ -68,12 +68,14 @@ function findScriptWithSchema($) {
   // Iterate over each script tag
   for (let i = 0; i < scriptTags.length; i++) {
     const scriptContent = $(scriptTags[i]).text();
+    console.log(scriptContent);
 
     // It just looks for recipeInstructions, should be specific enough
     try {
       const schema = JSON.parse(scriptContent);
 
       if (schema && schema.recipeInstructions) {
+        console.log("got in");
         // Check if the script has the desired properties
         // You can add your specific condition here
         return schema;
