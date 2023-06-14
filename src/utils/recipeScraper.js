48cea31a208a4699d39020bb20dc91fbde629f04
Joe <joefuller042@gmail.com>
2023-06-14 12:30:55 +0100

    general purpose script finder

--- src/utils/recipeScraper.js
+++ src/utils/recipeScraper.js
@@ -67,6 +67,7 @@ function findScriptWithSchema($) {
 
   // Iterate over each script tag
   for (let i = 0; i < scriptTags.length; i++) {
+    console.log(i);
     const scriptContent = $(scriptTags[i]).html();
 
     // It just looks for recipeInstructions, should be specific enough
