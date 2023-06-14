cdc1562577cd906a571bc6a319512af41b117600
Joe <joefuller042@gmail.com>
2023-06-14 10:35:56 +0100

    testing recipe schema

--- src/utils/recipeScraper.js
+++ src/utils/recipeScraper.js
@@ -42,7 +42,7 @@ async function scrapeRecipeFromUrl(url) {
     //   recipeInfo = $(element);
     // });
 
-    const recipeInfo = $('[data-testid="page-schema"]');
+    const recipeInfo = $('[data-testid="page-schema"]')[0];
 
     console.log(recipeInfo);
     // console.log(recipeInfo.name);
